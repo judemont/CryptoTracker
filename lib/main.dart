106c5e82bf345ba04e3d93d7af7ee7514ab5b49b
JdM <julien@rmbi.ch>
2024-05-07 21:35:55 +0200

    fixed appname

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Reciper',
+        title: 'CryptoTracker',
         theme: catppuccinTheme(catppuccin.macchiato),
         home: const PagesLayout(child: Home()));
   }
