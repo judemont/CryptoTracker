b23c003c52bba581f39817fa0c8dd4bd8e041753
JdM <julien@rmbi.ch>
2024-05-09 17:24:54 +0200

    added bottomnavbar

--- lib/pages_layout.dart
+++ lib/pages_layout.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home.dart';
+
 class PagesLayout extends StatefulWidget {
   final Widget child;
+  final bool displayNavBar;
+  final int? currentSection;
 
   const PagesLayout({
     super.key,
     required this.child,
+    this.displayNavBar = true,
+    this.currentSection,
   });
 
   @override
@@ -13,15 +19,40 @@ class PagesLayout extends StatefulWidget {
 }
 
 class _PagesLayoutState extends State<PagesLayout> {
+  late int currentPageIndex;
+  late Widget currentChild;
+  List<Widget> pages = [
+    const Home(),
+    const Text("Work in progress"),
+    const Placeholder(),
+  ];
+
   @override
   void initState() {
     super.initState();
+    currentPageIndex = widget.currentSection ?? 0;
+    currentChild = widget.child;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: widget.child,
-    );
+        body: currentChild,
+        bottomNavigationBar: widget.displayNavBar
+            ? NavigationBar(
+                selectedIndex: currentPageIndex,
+                onDestinationSelected: (index) => setState(() {
+                  currentPageIndex = index;
+                  currentChild = pages[currentPageIndex];
+                }),
+                destinations: const [
+                  NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+                  NavigationDestination(
+                      icon: Icon(Icons.star), label: "Favorites"),
+                  NavigationDestination(
+                      icon: Icon(Icons.settings), label: "Settings")
+                ],
+              )
+            : null);
   }
 }
