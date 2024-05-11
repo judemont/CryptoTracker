bcae59ac64f77ea327a69a100e3f83a20f48ec31
JdM <julien@rmbi.ch>
2024-05-11 21:54:49 +0200

    temp

---
+++ android/app/src/main/kotlin/com/example/cryptotracker/WidgetProvider.kt
@@ -0,0 +1,24 @@
+package jdm.apps.cryptotracker
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
+import es.antonborri.home_widget.HomeWidgetLaunchIntent
+import es.antonborri.home_widget.HomeWidgetProvider
+
+class AppWidgetProvider : HomeWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences) {
+        appWidgetIds.forEach { widgetId ->
+            val views = RemoteViews(context.packageName, R.layout.widget_layout).apply {
+                // Pending intent to update counter on button click
+                val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(context,
+                        Uri.parse("myAppWidget://updatecounter"))
+                setOnClickPendingIntent(R.id.bt_update, backgroundIntent)
+            }
+            appWidgetManager.updateAppWidget(widgetId, views)
+        }
+    }
+}
