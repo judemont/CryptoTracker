37a6efbd4f83172f3fc5ccbee6767857c281282b
judemont <julien@rmbi.ch>
2024-11-28 18:05:36 +0100

    Add News model to represent news articles with relevant fields

---
+++ lib/models/news.dart
@@ -0,0 +1,15 @@
+class News {
+  String? title;
+  String? source;
+  String? imgUrl;
+  DateTime? feedDate;
+  String? url;
+
+  News({
+    this.title,
+    this.source,
+    this.imgUrl,
+    this.feedDate,
+    this.url,
+  });
+}
